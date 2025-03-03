af5bbce1775db3d504430388267618dc89938ebc
shiv pratap <spsinghrajawat49@gmail.com>
2025-03-04 00:30:12 +0530

    port number edit

--- server/app.py
+++ server/app.py
@@ -109,5 +109,5 @@ def allrounder_prediction():
     except Exception as e:
         return f"Error: {str(e.with_traceback())}"
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port='5000')
 
